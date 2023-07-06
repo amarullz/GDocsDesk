74f09826830a201376816e91a1771c7b330416d0
Ahmad <amarullz@gmail.com>
2023-07-07 03:33:49 +0700

    Add code for virtual mouse handler

--- app/src/main/java/com/amarullz/app/googledocs/DocsView.java
+++ app/src/main/java/com/amarullz/app/googledocs/DocsView.java
@@ -3,6 +3,11 @@ package com.amarullz.app.googledocs;
 import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.os.Build;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.view.InputDevice;
+import android.view.MotionEvent;
+import android.view.PointerIcon;
 import android.view.View;
 import android.view.WindowManager;
 import android.webkit.CookieManager;
@@ -10,11 +15,16 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class DocsView {
   /* WebView Variables */
@@ -25,6 +35,14 @@ public class DocsView {
   private final String userAgentLogin;
   private final String userAgentApps;
   private boolean onLogin =false;
+
+  /* Cursor Related Variables */
+  public final ImageView cursor;
+  public final RelativeLayout webLayout;
+  private PointerIcon pointerActive;
+  private final ArrayList<PointerIcon> pointers;
+  private int mouseX=0;
+  private int mouseY=0;
 
   /* Settings & Floating Actions Variables */
   private boolean fabOpened=false;
@@ -163,6 +181,18 @@ public class DocsView {
     userAgentApps = activity.getResources().getString(R.string.uag_apps);
     setUAG(true);
 
+    /* Init Cursor Pointer */
+    cursor=activity.findViewById(R.id.cursor);
+    webLayout=activity.findViewById(R.id.weblayout);
+    pointers = new ArrayList<>();
+    pointers.add(PointerIcon.getSystemIcon(activity,PointerIcon.TYPE_DEFAULT));
+    pointers.add(PointerIcon.getSystemIcon(activity,PointerIcon.TYPE_TEXT));
+    pointers.add(PointerIcon.getSystemIcon(activity,PointerIcon.TYPE_HAND));
+    pointers.add(PointerIcon.getSystemIcon(activity,PointerIcon.TYPE_HORIZONTAL_DOUBLE_ARROW));
+    pointers.add(PointerIcon.getSystemIcon(activity,PointerIcon.TYPE_VERTICAL_DOUBLE_ARROW));
+    pointerActive=pointers.get(0);
+    initVirtualMouse();
+
     /* WebView Settings */
     CookieManager.getInstance().setAcceptCookie(true);
     CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
@@ -226,7 +256,153 @@ public class DocsView {
         return false;
       }
     });
-
     webView.loadUrl(url);
+  }
+
+  private void updatePointerIcon(){
+    try {
+      PointerIcon pi = webView.getPointerIcon();
+      if (pi == null) return;
+      if (!pi.equals(pointerActive)) {
+        int pos = pointers.indexOf(pi);
+        if (pos >= 0) {
+          switch (pos) {
+            case 1:
+              cursor.setImageResource(R.drawable.cursor_text);
+              break;
+            case 2:
+              cursor.setImageResource(R.drawable.cursor_hand);
+              break;
+            case 3:
+              cursor.setImageResource(R.drawable.cursor_hsize);
+              break;
+            case 4:
+              cursor.setImageResource(R.drawable.cursor_vsize);
+              break;
+            default:
+              cursor.setImageResource(R.drawable.cursor_default);
+              break;
+          }
+        }
+        pointerActive = pi;
+      }
+    }catch (Exception ignored){}
+  }
+  private void setCursorPos(float x, float y){
+    cursor.setTranslationX(x);
+    cursor.setTranslationY(y);
+    updatePointerIcon();
+  }
+  private void setCursorVisibility(boolean visible){
+    cursor.setVisibility(visible?View.VISIBLE:View.INVISIBLE);
+  }
+  private int dpx(float dp) {
+    final float scale = activity.getResources().getDisplayMetrics().density;
+    return (int) (dp * scale + 0.5f);
+  }
+  private void mouseEv(int type, float x, float y,
+                      float scrollX, float scrollY, long elapsed){
+    try {
+      mouseX += dpx(x);
+      mouseY += dpx(y);
+      if (mouseX < 0) mouseX = 0;
+      if (mouseY < 0) mouseY = 0;
+      if (mouseX > webView.getWidth()) mouseX = webView.getWidth();
+      if (mouseY > webView.getHeight()) mouseY = webView.getHeight();
+
+      MotionEvent ev;
+      long tick = new Date().getTime();
+      long downTime = tick - elapsed;
+      int metaState = 0;
+      int buttonState = 0;
+      byte actionButton = 0;
+      int action = -1;
+
+      MotionEvent.PointerCoords[] mPos = new MotionEvent.PointerCoords[1];
+      mPos[0] = new MotionEvent.PointerCoords();
+      mPos[0].x = mouseX;
+      mPos[0].y = mouseY;
+      mPos[0].pressure = 1;
+      mPos[0].size = 0;
+
+      MotionEvent.PointerProperties[] mProp = new MotionEvent.PointerProperties[1];
+      mProp[0] = new MotionEvent.PointerProperties();
+      mProp[0].id = 0;
+      mProp[0].toolType = MotionEvent.TOOL_TYPE_MOUSE;
+
+      if (type == 0) {
+        // MOVE
+        action = MotionEvent.ACTION_HOVER_MOVE;
+      } else if (type == 1) {
+        // MOUSE DOWN
+        actionButton = MotionEvent.BUTTON_PRIMARY;
+        action = MotionEvent.ACTION_BUTTON_PRESS;
+        buttonState = 0x8000000;
+      } else if (type == 2) {
+        // MOUSE UP
+        actionButton = MotionEvent.BUTTON_PRIMARY;
+        action = MotionEvent.ACTION_BUTTON_RELEASE;
+        buttonState = 0x8000000;
+      } else if (type == 3) {
+        // MOUSE DOWN SECONDARY
+        actionButton = MotionEvent.BUTTON_SECONDARY;
+        action = MotionEvent.ACTION_BUTTON_PRESS;
+        buttonState = 0x8000000;
+      } else if (type == 4) {
+        // MOUSE UP SECONDARY
+        actionButton = MotionEvent.BUTTON_SECONDARY;
+        action = MotionEvent.ACTION_BUTTON_RELEASE;
+        buttonState = 0x8000000;
+      } else if (type == 5) {
+        // DRAG
+        action = MotionEvent.ACTION_MOVE;
+        buttonState = MotionEvent.BUTTON_PRIMARY;
+      } else if (type == 6) {
+        // DRAG START
+        action = MotionEvent.ACTION_DOWN;
+        buttonState = MotionEvent.BUTTON_PRIMARY;
+      } else if (type == 7) {
+        // DRAG END
+        action = MotionEvent.ACTION_UP;
+        buttonState = MotionEvent.BUTTON_PRIMARY;
+      } else if (type == 8) {
+        // SCROLL
+        action = MotionEvent.ACTION_SCROLL;
+        mPos[0].setAxisValue(MotionEvent.AXIS_HSCROLL, scrollX / dpx(20));
+        mPos[0].setAxisValue(MotionEvent.AXIS_VSCROLL, scrollY / dpx(20));
+      }
+
+      ev = MotionEvent.obtain(
+          downTime, tick,
+          action, 1, mProp, mPos,
+          metaState, buttonState,
+          1f, 1f, 0x19238347, 0,
+          InputDevice.SOURCE_MOUSE, 0
+      );
+
+      if (actionButton == 1 || actionButton == 2) {
+        Parcel p = Parcel.obtain();
+        ev.writeToParcel(p, Parcelable.PARCELABLE_WRITE_RETURN_VALUE);
+        p.setDataPosition(0);
+        ev.recycle();
+        byte[] buf = p.marshall();
+        buf[84] = buf[68] = actionButton;
+        p.unmarshall(buf, 0, buf.length);
+        p.setDataPosition(0);
+        ev = MotionEvent.CREATOR.createFromParcel(p);
+        p.recycle();
+      }
+      if (type >= 5 && type <= 7)
+        webView.dispatchTouchEvent(ev);
+      else
+        webView.dispatchGenericMotionEvent(ev);
+      // runOnUiThread(()->setCursorPos(mouseX, mouseY));
+
+      setCursorPos(mouseX, mouseY);
+    }catch (Exception ignored){}
+  }
+
+  private void initVirtualMouse(){
+
   }
 }
