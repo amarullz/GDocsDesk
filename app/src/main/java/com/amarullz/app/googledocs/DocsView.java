4e43bdadeb7c480aefc6d2db128690051f7705c0
Ahmad <amarullz@gmail.com>
2023-07-13 15:16:44 +0700

    Softkey adjustPan on all activity

--- app/src/main/java/com/amarullz/app/googledocs/DocsView.java
+++ app/src/main/java/com/amarullz/app/googledocs/DocsView.java
@@ -153,9 +153,6 @@ public class DocsView {
           | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
           | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
           | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-          | View.SYSTEM_UI_FLAG_IMMERSIVE
-          | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-          | View.SYSTEM_UI_FLAG_LOW_PROFILE
           | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
       decorView.setSystemUiVisibility(uiOptions);
 
